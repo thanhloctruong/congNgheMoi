56ede3db768d98323e25d1e8d9c0b38df26ba624
ACan <thanhloctruong102@gmail.com>
2021-09-03 14:58:27 +0700

    add packed.js of server

--- frontend-website/src/screens/HomeScreen.js
+++ frontend-website/src/screens/HomeScreen.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import data from '../data';
-import Products from '../components/Products';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Products from "../components/Products";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
 function HomeScreen(props) {
-    return (
+  const [products, setProducts] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const [ error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () =>{
+        try {
+            setLoading(true);
+            const {data} = await axios.get('/api/products');
+            setLoading(false);
+            setProducts(data);    
+        } catch (err) {
+            setError(err.message);
+            setLoading(false);
+        }
+        
+    };
+    fetchData();
+}, []);
+  return (
+    <div>
+    {loading ? <LoadingBox /> 
+    :
+     error ? <MessageBox variant="danger"> {error} </MessageBox> 
+     :
         <div class="row center">
-            {
-                data.products.map((product) => (
-                 <Products key={product._id} product={product}/>
-                ))
-            }
+        {products.map(product => (
+            <Products key={product._id} product={product} />
+        ))}
         </div>
-    );
+     }
+    </div>
+     
+  );
 }
 
 export default HomeScreen;
